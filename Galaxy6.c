f132bcdb5264eb3df96c15d5fcf12df453e254c2
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-03-18 12:08:16 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int i,j;     for (i=1; i<=a; i++)     {         for (j=0; j<i; j++)         {             printf("*");         }         printf(" ");     }     return 0; }

---
+++ Galaxy6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int i,j;
+    for (i=1; i<=a; i++)
+    {
+        for (j=0; j<i; j++)
+        {
+            printf("*");
+        }
+        printf("
+");
+    }
+    return 0;
+}
